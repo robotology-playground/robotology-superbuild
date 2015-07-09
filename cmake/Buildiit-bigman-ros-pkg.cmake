40947dff9a78b22174643ec5bfc877b45fb5fa9b
Luca Muratore <luca.muratore@iit.it>
2015-07-09 16:41:00 +0200

    Added a commented Buildiit-bigman-ros-pkg.cmake waiting for the open-source

---
+++ cmake/Buildiit-bigman-ros-pkg.cmake
@@ -0,0 +1,9 @@
+## iit-bigman-ros-pkg
+#include(YCMEPHelper)
+
+#ycm_ep_helper(iit-bigman-ros-pkg TYPE GIT
+				      #STYLE GITHUB
+				      #REPOSITORY EnricoMingo/iit-coman-ros-pkg.git
+				      #TAG master
+				      #COMPONENT robots
+				      #INSTALL_COMMAND "")
