e8ed07c8cc92fa6bde612bbc99d872188eaba0a2
Enrico Mingo <enrico.mingo@iit.it>
2015-07-22 18:22:02 +0200

    Added simple_homing and centralized_inverse_kinematics

---
+++ cmake/Buildsimple_homing.cmake
@@ -0,0 +1,20 @@
+# simple_homing
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_package(Eigen QUIET)
+
+find_or_build_package(YARP QUIET)
+find_or_build_package(ICUB QUIET)
+find_or_build_package(GYM QUIET)
+find_or_build_package(idynutils QUIET)
+
+ycm_ep_helper(simple_homing TYPE GIT
+                            STYLE GITLAB_ROBOTOLOGY
+                            REPOSITORY walkman-drc/simple_homing.git
+                            TAG master
+                            COMPONENT drc
+                            DEPENDS YARP
+                                    ICUB
+                                    GYM
+                                    idynutils)
