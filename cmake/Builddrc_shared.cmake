3f7a876ccce953d17703ed4518e73e757227b1eb
Enrico Mingo <enrico.mingo@iit.it>
2015-07-22 18:04:40 +0200

    Added dependencies from drc.

---
+++ cmake/Builddrc_shared.cmake
@@ -0,0 +1,21 @@
+# drc_shared
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_package(catkin QUIET)
+find_package(roscpp QUIET)
+
+find_or_build_package(YARP 2.3.62.0 QUIET)
+find_or_build_package(iDynTree QUIET)
+find_or_build_package(paramHelp QUIET)
+
+ycm_ep_helper(drc_shared TYPE GIT
+                         STYLE GITLAB_ROBOTOLOGY
+                         REPOSITORY walkman-drc/drc_shared.git
+                         TAG master
+                         COMPONENT drc
+#                         TEST_COMMAND ctest
+                         DEPENDS YARP
+				                 IITComanRosPkg
+                                 iDynTree
+                                 paramHelp)
