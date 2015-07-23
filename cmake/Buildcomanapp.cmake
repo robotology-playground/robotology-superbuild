04621722c2e115eed4d3c1dc338e8f0572a7c55e
Luca Muratore <luca.muratore@iit.it>
2015-07-23 18:10:21 +0200

    Added coman repos to make the robot work.

---
+++ cmake/Buildcomanapp.cmake
@@ -0,0 +1,14 @@
+# ComanApp
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP QUIET)
+find_or_build_package(ICUB QUIET)
+
+ycm_ep_helper(comanapp TYPE GIT
+                    STYLE GITLAB_ROBOTOLOGY
+                    REPOSITORY walkman-drc/comanapp.git
+                    TAG master
+                    COMPONENT robots
+                    DEPENDS YARP
+                            ICUB)
