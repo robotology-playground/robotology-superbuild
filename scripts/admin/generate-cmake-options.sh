ae7615ddf43c7fc40188e8564b9af34e10b4eb54
Luca Muratore <luca.muratore@iit.it>
2015-07-23 18:12:36 +0200

    Removed admin scripts.

--- scripts/admin/generate-cmake-options.sh
+++
@@ -1,9 +0,0 @@
-### parameters are 
-# $1: hostname (could be also buildtype)
-# $2: os type (macos, winxp, lenny, etch, karmic ...)
-# $3: test type: nightly continuous experimental
-
-CMAKE_OPTIONS="\
--DNON_INTERACTIVE_BUILD:BOOL=TRUE \
--DBUILD_TESTING:BOOL=TRUE \
-"
