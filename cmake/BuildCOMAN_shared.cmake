5cc88b58c73657a47ff7d83cd66fc165b02ad4ff
Luca Muratore <luca.muratore@iit.it>
2015-07-23 18:08:33 +0200

    Added COMAN_shared

---
+++ cmake/BuildCOMAN_shared.cmake
@@ -0,0 +1,13 @@
+# COMAN_shared
+include(YCMEPHelper)
+
+find_package(YamlCpp QUIET)
+if(COMMAND set_package_properties)
+    set_package_properties(YamlCpp PROPERTIES PURPOSE "Used by COMAN_shared")
+endif()
+
+ycm_ep_helper(COMAN_shared TYPE GIT
+                           STYLE GITLAB_ROBOTOLOGY
+                           REPOSITORY walkman-drc/coman_shared.git
+                           TAG emergency
+                           COMPONENT external)
