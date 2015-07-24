4031fefc23f5f7c24444ecbb758228de74f86578
Luca Muratore <luca.muratore@iit.it>
2015-07-24 09:29:47 +0200

    iDynTree does not depends on ICUB anymore

--- cmake/BuildiDynTree.cmake
+++ cmake/BuildiDynTree.cmake
@@ -3,10 +3,7 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
-find_or_build_package(ICUB QUIET)
 find_or_build_package(orocos_kdl QUIET)
-find_or_build_package(kdl_codyco QUIET)
-find_or_build_package(kdl_format_io QUIET)
 
 
 ycm_ep_helper(iDynTree TYPE GIT
@@ -15,7 +12,4 @@ ycm_ep_helper(iDynTree TYPE GIT
               TAG master
               COMPONENT external
               DEPENDS YARP
-                      ICUB
-                      orocos_kdl
-                      kdl_codyco
-                      kdl_format_io)
+                      orocos_kdl)
