b0bf57d5b6f6ec6d10a9be8f1bb9ac12395ac33a
Luca Muratore <luca.muratore@iit.it>
2015-07-25 10:23:34 +0200

    Removed useless ProjectTags and updated the iDynTree TAG to master.

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -1,21 +1,14 @@
 
 set(GazeboYARPPlugins_TAG master)
-set(hrl_geom_TAG a8683c8)
-set(iDynTree_TAG 895f2f1)
-set(kdl_codyco_TAG 12bbb14)
-set(kdl_format_io_TAG 6890e69)
-set(mexWholeBodyModel_TAG 5c0edee)
+set(iDynTree_TAG master)
 set(paramHelp_TAG de9f652)
-set(pykdl_utils_TAG a8683c8)
-set(TinyXML_TAG 8459459)
-set(WBIToolbox_TAG e410c34)
-set(wholeBodyInterface_TAG 68db004)
 set(YARP_TAG master)
-set(yarpWholeBodyInterface_TAG 6d70ce2)
 set(YCM_TAG b327e75)
 set(roboptim-core_TAG v3.1)
+# moveit
 set(roboptim-core-plugin-ipopt_TAG v3.1)
-set(roboptim-capsule_TAG v3.1)
+# set(roboptim-capsule_TAG v3.1)
+# capsule gen
 
 set(idynutils_TAG master)
 set(OpenSoT_TAG devel)
@@ -25,7 +18,13 @@ set(drc_shared_TAG master)
 set(walking_TAG master)
 set(simple_homing_TAG VVV15)
 set(centralized_inverse_kinematics_TAG master)
+# COMAN robot
 set(comanapp_TAG VVV15)
 set(comaninterface_TAG VVV15)
 set(comanmod_TAG VVV15)
 set(COMAN_shared_TAG emergency)
+# COMAN model
+set(IITComanRosPkg_TAG master)
+
+# iCub model
+set(icub_gazebo_TAG master)
