604f3f6aa51f8f9a15076f4145f9cbbda1e7a236
Luca Muratore <luca.muratore@iit.it>
2015-07-25 11:10:38 +0200

    cmake files clenup

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -1,30 +1,39 @@
-
+############
+# external #
+############
+set(COMAN_shared_TAG emergency)
 set(GazeboYARPPlugins_TAG master)
+set(GYM_TAG master)
 set(iDynTree_TAG master)
-set(paramHelp_TAG de9f652)
-set(YARP_TAG master)
-set(YCM_TAG b327e75)
-set(roboptim-core_TAG v3.1)
+set(idynutils_TAG master)
 # moveit
+set(OpenSoT_TAG devel)
+set(paramHelp_TAG de9f652)
+set(roboptim-core_TAG v3.1)
 set(roboptim-core-plugin-ipopt_TAG v3.1)
 # set(roboptim-capsule_TAG v3.1)
 # capsule gen
-
-set(idynutils_TAG master)
-set(OpenSoT_TAG devel)
-set(yarp_ros_joint_state_publisher_TAG VVV15)
 set(robot_state_publisher_ext_TAG VVV15)
-set(drc_shared_TAG master)
-set(walking_TAG master)
-set(simple_homing_TAG VVV15)
+set(YARP_TAG master)
+set(yarp_ros_joint_state_publisher_TAG VVV15)
+set(YCM_TAG b327e75)
+
+
+##########
+# robots #
+##########
 set(centralized_inverse_kinematics_TAG master)
-# COMAN robot
 set(comanapp_TAG VVV15)
 set(comaninterface_TAG VVV15)
 set(comanmod_TAG VVV15)
-set(COMAN_shared_TAG emergency)
-# COMAN model
-set(IITComanRosPkg_TAG master)
-
-# iCub model
+set(drc_shared_TAG master)
 set(icub_gazebo_TAG master)
+set(iit-bigman-ros-pkg_TAG master)
+set(IITComanRosPkg_TAG master)
+set(simple_homing_TAG VVV15)
+set(walking_TAG master)
+
+
+
+
+
