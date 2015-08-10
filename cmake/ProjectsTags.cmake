b20a8813d886f2358358ad6c3123bbe62f998fd2
Alessio Rocchi <alessio.rocchi@iit.it>
2015-08-10 11:44:35 -0400

    ProjectsTags clenaup

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -1,70 +1,28 @@
 ############
 # external #
 ############
-set(COMAN_shared_TAG emergency)
+set(drc_shared_TAG master)
 set(GazeboYARPPlugins_TAG master)
-<<<<<<< HEAD
 set(hrl_geom_TAG a8683c8)
-set(iDynTree_TAG 895f2f1)
-set(kdl_codyco_TAG 12bbb14)
-set(kdl_format_io_TAG 6890e69)
+set(iDynTree_TAG master)
+set(idynutils_TAG master)
 set(mexWholeBodyModel_TAG 5c0edee)
+set(OpenSoT_TAG master)
 set(paramHelp_TAG de9f652)
 set(pykdl_utils_TAG a8683c8)
-set(TinyXML_TAG 8459459)
-set(WBIToolbox_TAG e410c34)
-set(wholeBodyInterface_TAG 68db004)
-set(YARP_TAG a2b38a0)
-set(yarpWholeBodyInterface_TAG 6d70ce2)
-set(YCM_TAG b327e75)
 set(roboptim-core_TAG v3.1)
 set(roboptim-core-plugin-ipopt_TAG v3.1)
 set(roboptim-capsule_TAG v3.1)
-
-set(idynutils_TAG master)
-set(OpenSoT_TAG devel)
-set(yarp_ros_joint_state_publisher_TAG VVV15)
-set(robot_state_publisher_ext_TAG VVV15)
-set(drc_shared_TAG master)
-set(walking_TAG master)
-set(simple_homing_TAG master)
-=======
-set(GYM_TAG master)
-set(iDynTree_TAG master)
-set(idynutils_TAG master)
-# moveit
-set(OpenSoT_TAG devel)
-set(paramHelp_TAG de9f652)
-set(roboptim-core_TAG v3.1)
-set(roboptim-core-plugin-ipopt_TAG v3.1)
-# set(roboptim-capsule_TAG v3.1)
-# capsule gen
-set(robot_state_publisher_ext_TAG VVV15)
-set(YARP_TAG master)
-set(yarp_ros_joint_state_publisher_TAG VVV15)
+set(TinyXML_TAG 8459459)
+set(WBIToolbox_TAG master)
+set(wholeBodyInterface_TAG master)
+set(YARP_TAG a2b38a0)
+set(yarpWholeBodyInterface_TAG 6d70ce2)
 set(YCM_TAG b327e75)
-
 
 ##########
 # robots #
 ##########
->>>>>>> 604f3f6... cmake files clenup
-set(centralized_inverse_kinematics_TAG master)
-set(comanapp_TAG VVV15)
-set(comaninterface_TAG VVV15)
-set(comanmod_TAG VVV15)
-<<<<<<< HEAD
-set(COMAN_shared_TAG emergency)
-=======
-set(drc_shared_TAG master)
 set(icub_gazebo_TAG master)
-set(iit-bigman-ros-pkg_TAG master)
 set(IITComanRosPkg_TAG master)
-set(simple_homing_TAG VVV15)
-set(walking_TAG master)
 
-
-
-
-
->>>>>>> 604f3f6... cmake files clenup
