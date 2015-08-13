ce64aff644cb927f8d6af877765087c7e0c63e8d
Alessio Rocchi <alessio.rocchi@iit.it>
2015-08-13 13:46:01 -0400

    Fixed get_dependencies.sh on 12.04

--- scripts/get_dependencies.sh
+++ scripts/get_dependencies.sh
@@ -36,7 +36,7 @@ else
               git subversion doxygen graphviz  \
               libace-dev libgsl0-dev libgtkmm-2.4-dev libgoocanvasmm-dev libsqlite3-dev python3.2-dev swig  \
               icub-common coinor-libipopt-dev gfortran libtinyxml2-dev \
-              libeigen3-dev libxml2-dev  \
+              libeigen3-dev libxml-dev  \
               ros-hydro-roscpp-core ros-hydro-srdfdom ros-hydro-cmake-modules  \
               ros-hydro-openni2-*  ros-hydro-moveit-full ros-hydro-joy*  \
               ros-hydro-urdfdom-py ros-hydro-libg2o ros-hydro-octomap* \
