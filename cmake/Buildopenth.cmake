6feccf5486313538616a33d792266dd2d7872656
Enrico Mingo <enrico.mingo@iit.it>
2015-08-24 10:44:17 +0900

    Updated superbuild

--- cmake/Buildopenth.cmake
+++ cmake/Buildopenth.cmake
@@ -1,12 +1,12 @@
-# ynl-hydra-ros-pkg
+# openth
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(moveit_compute_default_collisions REQUIRED QUIET)
 
-ycm_ep_helper(ynl-hydra-ros-pkg TYPE GIT
+ycm_ep_helper(openth TYPE GIT
                              STYLE GITHUB
-                             REPOSITORY asd
+                             REPOSITORY OpenTheta/openth.git
                              TAG master
                              COMPONENT robots
                              DEPENDS moveit_compute_default_collisions
--- robotology-setup.bash
+++ robotology-setup.bash
@@ -50,9 +50,11 @@ pathadd GAZEBO_MODEL_PATH $ROBOTOLOGY_ROOT/robots/icub_gazebo
 if [ -d $ROBOTOLOGY_ROOT/robots/iit-bigman-ros-pkg ]; then
         pathadd ROS_PACKAGE_PATH $ROBOTOLOGY_ROOT/robots/iit-bigman-ros-pkg
 	pathadd GAZEBO_MODEL_PATH $ROBOTOLOGY_ROOT/robots/iit-bigman-ros-pkg/bigman_gazebo/database
+fi
 
-	pathadd ROS_PACKAGE_PATH $ROBOTOLOGY_ROOT/robots/ynl-hydra-ros-pkg
-	pathadd GAZEBO_MODEL_PATH $ROBOTOLOGY_ROOT/robots/ynl-hydra-ros-pkg/hydra_gazebo/database
+if [ -d $ROBOTOLOGY_ROOT/robots/openth/ynl-hydra-ros-pkg ]; then
+	pathadd ROS_PACKAGE_PATH $ROBOTOLOGY_ROOT/robots/openth/ynl-hydra-ros-pkg
+	pathadd GAZEBO_MODEL_PATH $ROBOTOLOGY_ROOT/robots/openth/ynl-hydra-ros-pkg/hydra_gazebo/database
 fi
 
 export COMAN_ROOT=${ROBOTOLOGY_ROOT}/build/install
