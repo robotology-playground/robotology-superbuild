16446f6a5f7af7dff3f71dad0dc6edab8aa87509
Luca Muratore <luca.muratore@iit.it>
2015-11-12 13:25:12 +0100

    drc_valve depends on OpenSoT

--- cmake/Builddrc_valve.cmake
+++ cmake/Builddrc_valve.cmake
@@ -4,6 +4,7 @@ include(FindOrBuildPackage)
 
 find_or_build_package(drc_shared QUIET)
 find_or_build_package(GYM QUIET)
+find_or_build_package(OpenSoT QUIET)
 find_or_build_package(trajectory_generator QUIET)
 
 ycm_ep_helper(  drc_valve 
@@ -14,5 +15,6 @@ ycm_ep_helper(  drc_valve
                 COMPONENT robots
                 DEPENDS drc_shared
                         GYM
+                        OpenSoT
                         trajectory_generator
-	     )
+            )
