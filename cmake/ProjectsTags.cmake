718216f95795da9d2206d2538a1548a03def160a
Luca Muratore <luca.muratore@iit.it>
2015-11-20 13:02:52 +0100

    Set the TAG to hash and no to branch: in this way the make update target is going to work.

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -1,14 +1,14 @@
 #superbuild
-set(YCM_TAG b327e75)
+set(YCM_TAG b327e753)
 
 # Superbuild CORE
 set(YARP_TAG 2fae615)
 set(paramHelp_TAG de9f652)
 set(GazeboYARPPlugins_TAG 0b4a645)
 set(iDynTree_TAG f0346c1)
-set(idynutils_TAG master)
-set(OpenSoT_TAG master)
-set(GYM_TAG master)
+set(idynutils_TAG 79825b3d2)
+set(OpenSoT_TAG 101b9d703)
+set(GYM_TAG e49bb522a)
 
 # CAPSULE
 set(roboptim-core_TAG v3.1)
@@ -16,19 +16,20 @@ set(roboptim-core-plugin-ipopt_TAG v3.1)
 set(roboptim-capsule_TAG v3.1)
 
 # utils
-set(drc_shared_TAG master)
-
-# modules
-set(centralized_inverse_kinematics_TAG master)
-set(simple_homing_TAG master)
-set(walking_TAG master)
+set(drc_shared_TAG e995308796b)
+set(trajectory_generator TAG 4bcf25603c)
 
 # ROS publisher
-set(robot_state_publisher_ext_TAG master)
-set(yarp_ros_joint_state_publisher_TAG master)
+#set(robot_state_publisher_ext_TAG master)
+#set(yarp_ros_joint_state_publisher_TAG master)
+
+# modules
+#set(centralized_inverse_kinematics_TAG master)
+#set(simple_homing_TAG master)
+#set(walking_TAG master)
 
 # COMAN
-set(comanapp_TAG VVV15)
-set(comaninterface_TAG VVV15)
-set(comanmod_TAG VVV15)
-set(COMAN_shared_TAG emergency)
+#set(comanapp_TAG VVV15)
+#set(comaninterface_TAG VVV15)
+#set(comanmod_TAG VVV15)
+#set(COMAN_shared_TAG emergency)
