a4ee9e1800af46189e3f7f94f8a823259c97f90d
Alessio Rocchi <alessio.rocchi@iit.it>
2015-12-11 19:33:20 +0100

    Fixed simulation/activate-remote.sh to work with remote simulation machine

--- profiles/simulation/activate-remote.sh
+++ profiles/simulation/activate-remote.sh
@@ -10,6 +10,11 @@ echo " Remember you need to use 169.254.89.x as local IP address"
 echo " with x < 20 and x != 10"
 if [ $# -eq 0 ]; then
     echo "169.254.89.10 10000 yarp" > `yarp conf`
+    echo "export GAZEBO_MASTER_URI=http://169.254.89.10:11345" >> $ROBOTOLOGY_ROOT/build/active_profile
 else
     echo "$1 10000 yarp" > `yarp conf`
+    echo "export GAZEBO_MASTER_URI=http://${1}:11345" >> $ROBOTOLOGY_ROOT/build/active_profile
+    echo "export ROS_MASTER_URI=http://${1}::11311" >> $ROBOTOLOGY_ROOT/build/active_profile
+    echo "export ROS_HOSTNAME=${1}.local" >> $ROBOTOLOGY_ROOT/build/active_profile
+
 fi
