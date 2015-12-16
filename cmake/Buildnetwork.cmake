08870dab591cd2217489925363f0ee05c167a0a9
Luca Muratore <luca.muratore@iit.it>
2015-12-16 18:39:32 +0100

    Added network repo to robotology-superbuild

---
+++ cmake/Buildnetwork.cmake
@@ -0,0 +1,9 @@
+# network
+include(YCMEPHelper)
+
+ycm_ep_helper(network TYPE GIT
+                      STYLE GITLAB_ROBOTOLOGY
+                      REPOSITORY walkman-drc/network.git
+                      TAG master
+                      COMPONENT external)
+
