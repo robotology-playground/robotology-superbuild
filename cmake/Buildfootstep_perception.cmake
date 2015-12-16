19d1d71095b43120c024d0580d649c2c9b09d25c
Luca Muratore <luca.muratore@iit.it>
2015-12-16 18:39:52 +0100

    Added footstep_perception repo to robotology-superbuild

---
+++ cmake/Buildfootstep_perception.cmake
@@ -0,0 +1,22 @@
+# footstep_perception
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_package(catkin QUIET)
+find_package(roscpp QUIET)
+find_package(rospy QUIET)
+find_package(Boost COMPONENTS system QUIET)
+find_package(Eigen QUIET)
+find_or_build_package(drc_shared QUIET)
+find_or_build_package(GYM QUIET)
+find_or_build_package(iDynTree QUIET)
+
+ycm_ep_helper(footstep_perception TYPE GIT
+                                  STYLE GITLAB_ROBOTOLOGY
+                                  REPOSITORY walkman-drc/footstep_perception.git
+                                  TAG master
+                                  COMPONENT robots
+                                  DEPENDS YARP
+                                          drc_shared
+                                          GYM
+					  iDynTree)
