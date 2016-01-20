16a3de27d1cd91b57d53b1d1fc2fcddec709c7e4
Luca Muratore <luca.muratore@iit.it>
2016-01-20 14:48:33 +0100

    Added the rm of the conflict liburdf library inside pronto installation folder.

--- scripts/global/install_pronto.sh
+++ scripts/global/install_pronto.sh
@@ -3,5 +3,6 @@ cd $ROBOTOLOGY_ROOT/external/pronto/build
 cp -r bin/* $ROBOTOLOGY_ROOT/build/install/bin/
 cp -r include/* $ROBOTOLOGY_ROOT/build/install/include/
 cp -r lib/* $ROBOTOLOGY_ROOT/build/install/lib/
+rm -f $ROBOTOLOGY_ROOT/build/install/lib/liburdf.so # remove the conflict
 cp -r share/* $ROBOTOLOGY_ROOT/build/install/share/
 # I am not going to install the config and the models folders.
