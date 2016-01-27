d8589150e339c5d47db81dca7c6899fc10fc608b
Luca Muratore <luca.muratore@iit.it>
2016-01-27 12:40:54 +0100

    ft_publisher added in the robotology-superbuild

---
+++ cmake/Buildft_publisher.cmake
@@ -0,0 +1,15 @@
+# ft_publisher
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_package(catkin QUIET)
+find_package(roscpp QUIET)
+find_package(Eigen QUIET)
+find_or_build_package(GYM QUIET)
+
+ycm_ep_helper(ft_publisher TYPE GIT
+                           STYLE GITHUB
+                           REPOSITORY liesrock/ft_publisher.git 
+                           TAG master
+                           COMPONENT robots
+                           DEPENDS GYM)
