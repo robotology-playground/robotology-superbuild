320168bac3243ef9b7dff97aafb92e8337612679
Markus Grotz <markus.grotz@kit.edu>
2016-04-07 10:47:55 +0200

    updated commit version for YARP and GazeboYARPPlugin

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -2,9 +2,9 @@
 set(YCM_TAG b327e753)
 
 # Superbuild CORE: put the SHA1 of the commit or the TAG, not the branch name.
-set(YARP_TAG 2fae615)
+set(YARP_TAG bb889a)
 set(paramHelp_TAG de9f652)
-set(GazeboYARPPlugins_TAG caf5b45a8)
+set(GazeboYARPPlugins_TAG 63e776743)
 set(iDynTree_TAG f0346c1)
 set(idynutils_TAG 79825b3d2)
 set(OpenSoT_TAG 101b9d703)
