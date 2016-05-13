4cdc278e25ffc853baf632b9564ef3463e9c08b6
Alessandro <ale.settimi@gmail.com>
2016-05-13 14:40:19 +0200

    added drc_raise and pilot_interface as superbuild modules

---
+++ cmake/Builddrc_raise.cmake
@@ -0,0 +1,20 @@
+# Raise
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(drc_shared QUIET)
+find_or_build_package(GYM QUIET)
+find_or_build_package(OpenSoT QUIET)
+find_or_build_package(trajectory_generator QUIET)
+
+ycm_ep_helper(  drc_raise 
+                TYPE GIT
+                STYLE GITLAB_ROBOTOLOGY
+                REPOSITORY walkman-drc/drc_raise.git
+                TAG master
+                COMPONENT robots
+                DEPENDS drc_shared
+                        GYM
+                        OpenSoT
+                        trajectory_generator
+            )
---
+++ cmake/Buildpilot_interface.cmake
@@ -0,0 +1,18 @@
+# PI
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(drc_shared QUIET)
+find_or_build_package(GYM QUIET)
+find_or_build_package(trajectory_generator QUIET)
+
+ycm_ep_helper(  pilot_interface 
+                TYPE GIT
+                STYLE GITLAB_ROBOTOLOGY
+                REPOSITORY walkman-drc/pilot_interface.git
+                TAG master
+                COMPONENT robots
+                DEPENDS drc_shared
+                        GYM
+                        trajectory_generator
+            )
