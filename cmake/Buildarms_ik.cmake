df11208e4710c201e64c3601000db571752d5e1d
Alessandro <ale.settimi@gmail.com>
2016-05-14 23:54:29 +0200

    added arms_ik to the superbuild packages

---
+++ cmake/Buildarms_ik.cmake
@@ -0,0 +1,17 @@
+# arms_ik
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(drc_shared QUIET)
+find_or_build_package(GYM QUIET)
+find_or_build_package(OpenSoT QUIET)
+find_or_build_package(trajectory_generator QUIET)
+
+ycm_ep_helper(  arms_ik 
+                TYPE GIT
+                STYLE GITLAB_ROBOTOLOGY
+                REPOSITORY walkman-drc/arms_ik.git
+                TAG master
+                COMPONENT robots
+                DEPENDS drc_shared
+            )
