516260c8ca1ce9e4ebb608d222a510b13b7bd50e
Enrico Mingo <enrico.mingo@iit.it>
2016-05-27 18:02:33 +0200

    Added scripts related to rsb installation

---
+++ scripts/rsb/copy_rsb.conf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "This script will copy the file rsb.conf in your .config folder."
+
+cp rsb.conf ~/.config
---
+++ scripts/rsb/install_python_dep.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python pip install --user rsb-python
+python pip install --user rststable
+
+
