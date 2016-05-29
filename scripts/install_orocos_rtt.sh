22b2edefed546be22adaa061abb58208db762f62
Enrico Mingo <enrico.mingo@iit.it>
2016-05-29 13:50:49 +0200

    Added missing dep

--- scripts/install_orocos_rtt.sh
+++ scripts/install_orocos_rtt.sh
@@ -10,12 +10,14 @@ catkin_init_workspace
 
 export GIT_SSL_NO_VERIFY=1
 git clone --recursive -b toolchain-2.7 http://git.gitorious.org/orocos-toolchain/orocos_toolchain.git src
-git clone https://github.com/orocos/rtt_geometry.git src
+cd src
+git clone https://github.com/orocos/rtt_geometry.git
+cd ..
 
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROBOTOLOGY_ROOT/orocos_ws/src
 
 sudo apt-get install ruby ruby-dev
-sudo apt-get install omniorb-idl omniorb omniorb-nameserver
+sudo apt-get install omniorb-idl omniorb omniorb-nameserver omniidl
 sudo gem install rake hoe
 
 rosdep install orocos_toolchain
