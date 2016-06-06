fe2cb07cd84b605a122a9348d696f0f638cb8102
Enrico Mingo <enrico.mingo@iit.it>
2016-06-06 15:17:51 +0200

    Added sensors-description pkg

---
+++ cmake/Buildsensors-description.cmake
@@ -0,0 +1,10 @@
+# sensors-description
+include(YCMEPHelper)
+
+ycm_ep_helper(  sensors-description
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY EnricoMingo/sensors-description.git
+                TAG master
+                COMPONENT external
+            )
