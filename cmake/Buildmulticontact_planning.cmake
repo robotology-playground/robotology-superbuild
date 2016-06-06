72c67702e88837d69fdd49ab188b0adbed85247a
Alessandro <ale.settimi@gmail.com>
2016-06-06 16:42:59 +0200

    added multicontact_planning in the superbuild, as part of the WALKMAN DEMOS PROJECT

---
+++ cmake/Buildmulticontact_planning.cmake
@@ -0,0 +1,16 @@
+# multicontact_planning
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(drc_shared QUIET)
+find_or_build_package(idynutils QUIET)
+find_or_build_package(GYM QUIET)
+
+ycm_ep_helper(multicontact_planning 
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY CentroEPiaggio/multicontact_planning.git
+                TAG master
+                COMPONENT robots
+                DEPENDS drc_shared idynutils GYM
+)
