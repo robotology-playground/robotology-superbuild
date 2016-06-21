ae5dc59c7742644d1d4a6030cbcff1c2ac692cb3
Enrico Mingo <enrico.mingo@iit.it>
2016-06-21 14:55:15 +0200

    Added srdfdom_advr

---
+++ cmake/Buildsrdfdom_advr.cmake
@@ -0,0 +1,10 @@
+# srdfdom_advr
+include(YCMEPHelper)
+
+ycm_ep_helper(  srdfdom_advr
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY liesrock/srdfdom.git
+                TAG master
+                COMPONENT external
+            )
