93b6e1fb0d26000890c5ac9f2c17aa10fd95e559
Alessandro <ale.settimi@gmail.com>
2016-06-28 11:36:27 +0200

    added psearch_control_interface to the WALKMAN DEMOS projects

---
+++ cmake/Buildpsearch_control_interface.cmake
@@ -0,0 +1,16 @@
+# psearch_control_interface
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(drc_shared QUIET)
+find_or_build_package(GYM QUIET)
+find_or_build_package(trajectory_generator QUIET)
+
+ycm_ep_helper(  psearch_control_interface 
+                TYPE GIT
+                STYLE GITLAB_ROBOTOLOGY
+                REPOSITORY walkman-drc/psearch_control_interface.git
+                TAG master
+                COMPONENT robots
+                DEPENDS drc_shared
+            )
