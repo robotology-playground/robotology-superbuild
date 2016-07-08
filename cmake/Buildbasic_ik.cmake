1702824956d54b924f92bf48ff853a85d7af655b
Luca Muratore <luca.muratore@iit.it>
2016-07-08 10:11:38 +0200

    basic_ik cmake revisited

--- cmake/Buildbasic_ik.cmake
+++ cmake/Buildbasic_ik.cmake
@@ -3,12 +3,14 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(OpenSoT QUIET)
+find_or_build_package(GYM QUIET)
 
 ycm_ep_helper(  basic_ik
                 TYPE GIT
                 STYLE GITLAB_ROBOTOLOGY
                 REPOSITORY luca.muratore/basic_ik.git
                 TAG master
-                COMPONENT external
+                COMPONENT robots
                 DEPENDS OpenSoT
+			GYM
             )
