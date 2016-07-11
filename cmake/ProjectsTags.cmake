960e61bb65a70e44da12d5e6bcc8c9fd5917ad6b
Luca Muratore <luca.muratore@iit.it>
2016-07-11 12:41:58 +0200

    YARP version updated.

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -2,7 +2,7 @@
 set(YCM_TAG b327e753)
 
 # Superbuild CORE: put the SHA1 of the commit or the TAG, not the branch name.
-set(YARP_TAG 2fae615)
+set(YARP_TAG ecd0040)
 set(paramHelp_TAG de9f652)
 set(GazeboYARPPlugins_TAG caf5b45a8)
 set(iDynTree_TAG f0346c1)
