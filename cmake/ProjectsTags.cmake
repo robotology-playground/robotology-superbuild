e68aee0df2ce4b81f295345eb50508eabbb098d0
Luca Muratore <luca.muratore@iit.it>
2016-07-20 15:34:39 +0200

    OpenSoT TAG updated

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -7,7 +7,7 @@ set(paramHelp_TAG de9f652)
 set(GazeboYARPPlugins_TAG caf5b45a8)
 set(iDynTree_TAG f0346c1)
 set(idynutils_TAG 96972f0)
-set(OpenSoT_TAG f49c1720b)
+set(OpenSoT_TAG f992366773)
 set(GYM_TAG cc2e32787)
 
 # CAPSULE: put the SHA1 of the commit or the TAG, not the branch name.
