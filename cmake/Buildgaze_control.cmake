d7175e9e930302622844fb2bc4e040da1ba1289a
Alessandro <ale.settimi@gmail.com>
2016-07-27 11:04:05 +0200

    added gaze control to the walk-man demos projects

---
+++ cmake/Buildgaze_control.cmake
@@ -0,0 +1,15 @@
+# gaze_control
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(drc_shared QUIET)
+find_or_build_package(GYM QUIET)
+
+ycm_ep_helper(  gaze_control 
+                TYPE GIT
+                STYLE GITLAB_ROBOTOLOGY
+                REPOSITORY walkman-drc/gaze_control.git
+                TAG master
+                COMPONENT robots
+                DEPENDS drc_shared
+            )
