e9026db86e3a839ded7415d9fc7e3d7ecc1dee85
Alessandro <ale.settimi@gmail.com>
2016-08-25 11:33:38 +0200

    added a new wholebody ik library to the superbuild (it is part of the Walkman Demos projects)

---
+++ cmake/Buildwholebody_ik.cmake
@@ -0,0 +1,17 @@
+# wholebody_ik
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(drc_shared QUIET)
+find_or_build_package(idynutils QUIET)
+find_or_build_package(GYM QUIET)
+find_or_build_package(trajectory_generator QUIET)
+
+ycm_ep_helper(  wholebody_ik 
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY CentroEPiaggio/wholebody_ik.git
+                TAG master
+                COMPONENT robots
+                DEPENDS drc_shared idynutils GYM
+            )
