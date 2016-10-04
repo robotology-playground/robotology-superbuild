51e8f18c620cae43f308b6741ea487a49776a322
Luca Muratore <luca.muratore@iit.it>
2016-10-04 19:47:28 +0200

    XBotCoreModel renamed

--- cmake/BuildXBotCoreModel.cmake
+++ cmake/BuildXBotCoreModel.cmake
@@ -1,11 +1,14 @@
-# xbotcoremodel
+# XBotCoreModel
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-ycm_ep_helper(  xbotcoremodel
+find_or_build_package(srdfdom_advr QUIET)
+
+ycm_ep_helper(  XBotCoreModel
                 TYPE GIT
                 STYLE GITLAB_ROBOTOLOGY
                 REPOSITORY walkman-drc/xbotcoremodel.git
                 TAG master
                 COMPONENT external
+                DEPENDS srdfdom_advr
             )
