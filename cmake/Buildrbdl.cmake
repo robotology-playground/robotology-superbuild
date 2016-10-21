e285b2028b2356f3ee9c6cfb813d0f350f0ba559
Luca Muratore <luca.muratore@iit.it>
2016-10-21 16:29:52 +0200

    Added rbdl to CENTAURO sub project.

---
+++ cmake/Buildrbdl.cmake
@@ -0,0 +1,11 @@
+# rbdl 
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+ycm_ep_helper(  rbdl
+                TYPE HG
+                STYLE BITBUCKET
+                REPOSITORY rbdl/rbdl
+                TAG default
+                COMPONENT external
+            )
