e48e603851cb46726d77e47bf1b7a118186c18e7
Luca Muratore <luca.muratore@iit.it>
2016-10-21 16:48:31 +0200

    Updated XBotInterface remote

--- cmake/BuildXBotInterface.cmake
+++ cmake/BuildXBotInterface.cmake
@@ -7,7 +7,7 @@ find_or_build_package(XBotCoreModel QUIET)
 ycm_ep_helper(  XBotInterface
                 TYPE GIT
                 STYLE GITLAB_ADVRCLOUD
-                REPOSITORY lmuratore/xbotinterface.git
+                REPOSITORY ADVR_Humanoids/xbotinterface.git
                 TAG master
                 COMPONENT external
                 DEPENDS XBotCoreModel
