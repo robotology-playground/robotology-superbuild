83415ff60417733d2312137df4c9f2e50a5de096
Arturo Laurenzi <arturo.laurenzi@iit.it>
2016-10-25 23:01:33 +0200

    Fixed typo in cmake file for RobotInterfaceDUMMY

--- cmake/BuildRobotInterfaceDUMMY.cmake
+++ cmake/BuildRobotInterfaceDUMMY.cmake
@@ -2,7 +2,7 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-ycm_ep_helper(  RobotInterfaceROS
+ycm_ep_helper(  RobotInterfaceDUMMY
                 TYPE GIT
                 STYLE GITLAB_ADVRCLOUD
                 REPOSITORY advr_humanoids/robotinterfacedummy.git
