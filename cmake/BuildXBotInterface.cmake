eecbefb73a1ab378a2a2d33b69ce834f1e5d2935
Luca Muratore <luca.muratore@iit.it>
2016-11-04 18:28:27 +0100

    sharedlibpp instead of sharedlib++

--- cmake/BuildXBotInterface.cmake
+++ cmake/BuildXBotInterface.cmake
@@ -3,7 +3,7 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(XBotCoreModel QUIET)
-find_or_build_package(sharedlib++ QUIET)
+find_or_build_package(sharedlibpp QUIET)
 
 ycm_ep_helper(  XBotInterface
                 TYPE GIT
@@ -12,5 +12,5 @@ ycm_ep_helper(  XBotInterface
                 TAG master
                 COMPONENT external
                 DEPENDS XBotCoreModel
-			sharedlib++
+			sharedlibpp
              )
