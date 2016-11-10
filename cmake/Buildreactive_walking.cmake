22ebcb6d5048b1c27567f4076e0ebee048b1455e
Enrico Mingo <enrico.mingo@iit.it>
2016-11-10 17:16:53 +0100

    Updated build file

--- cmake/Buildreactive_walking.cmake
+++ cmake/Buildreactive_walking.cmake
@@ -7,7 +7,7 @@ find_or_build_package(XBotInterface QUIET)
 ycm_ep_helper(  reactive_walking
                 TYPE GIT
                 STYLE GITLAB_ADVRCLOUD
-                REPOSITORY advr_humanoids/reactive_walking.git
+                REPOSITORY walkman/reactive_walking.git
                 TAG master
                 COMPONENT external
                 DEPENDS XBotInterface
