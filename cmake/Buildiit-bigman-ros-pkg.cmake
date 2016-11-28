a8301500783d49efdfd19c808ba9c76d80654f80
Luca Muratore <luca.muratore@iit.it>
2016-11-28 21:01:02 +0100

    Removed moveit_compute_default_collisions

--- cmake/Buildiit-bigman-ros-pkg.cmake
+++ cmake/Buildiit-bigman-ros-pkg.cmake
@@ -2,7 +2,7 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-find_or_build_package(moveit_compute_default_collisions REQUIRED QUIET)
+#find_or_build_package(moveit_compute_default_collisions REQUIRED QUIET)
 
 ycm_ep_helper(  iit-bigman-ros-pkg
                 TYPE GIT
@@ -10,6 +10,6 @@ ycm_ep_helper(  iit-bigman-ros-pkg
                 REPOSITORY walkman-drc/iit-bigman-ros-pkg.git
                 TAG master
                 COMPONENT robots
-                DEPENDS moveit_compute_default_collisions
+               # DEPENDS moveit_compute_default_collisions
                 INSTALL_COMMAND ""
             )
