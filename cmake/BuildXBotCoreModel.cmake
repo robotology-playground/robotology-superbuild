e4f3543173953986473cb6a0bbfa8d2f8003a766
Luca Muratore <luca.muratore@iit.it>
2016-12-04 10:00:44 +0100

    Added missing dependency in XBotCoreModel

--- cmake/BuildXBotCoreModel.cmake
+++ cmake/BuildXBotCoreModel.cmake
@@ -3,6 +3,7 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(srdfdom_advr QUIET)
+find_or_build_package(XBotCore-interfaces QUIET)
 
 ycm_ep_helper(  XBotCoreModel
                 TYPE GIT
@@ -11,4 +12,5 @@ ycm_ep_helper(  XBotCoreModel
                 TAG centauro_bonn_review_2016
                 COMPONENT external
                 DEPENDS srdfdom_advr
+			XBotCore-interfaces
             )
