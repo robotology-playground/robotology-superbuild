881642cf93a9f1281452035ab6e8b4205c404ad2
Luca Muratore <luca.muratore@iit.it>
2017-01-18 16:56:46 +0100

    OpenSoT minimal version added to advr-core packages.

--- cmake/BuildOpenSoT.cmake
+++ cmake/BuildOpenSoT.cmake
@@ -2,8 +2,8 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-find_or_build_package(iDynTree QUIET)
-find_or_build_package(idynutils QUIET)
+#find_or_build_package(iDynTree QUIET)
+#find_or_build_package(idynutils QUIET)
 
 # Workaround to use qpOASES that does not have an install target or cmake config
 # files.
@@ -20,7 +20,7 @@ ycm_ep_helper(  OpenSoT TYPE GIT
                 REPOSITORY robotology-playground/OpenSoT.git
                 TAG devel
                 COMPONENT external
-                DEPENDS iDynTree
-                        idynutils
+		#             DEPENDS iDynTree
+		#       idynutils
                 CMAKE_CACHE_ARGS -DOPENSOT_COMPILE_TESTS:BOOL=ON
             )
