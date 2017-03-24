bc247f5152090aaf663f728ef285d4eac43a3a7a
Luca Muratore <luca.muratore@iit.it>
2017-03-24 19:38:30 +0100

    Added missing dependency to advr_humanoids_common_utils

--- cmake/Buildadvr_humanoids_common_utils.cmake
+++ cmake/Buildadvr_humanoids_common_utils.cmake
@@ -2,10 +2,13 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
+find_or_build_package_with_tag(srdfdom_advr QUIET)
+
 ycm_ep_helper(  advr_humanoids_common_utils
                 TYPE GIT
                 STYLE GITLAB_ADVRCLOUD
                 REPOSITORY advr_humanoids/advr_humanoids_common_utils.git
                 TAG master
+                DEPENDS srdfdom_advr
                 COMPONENT external
             )
