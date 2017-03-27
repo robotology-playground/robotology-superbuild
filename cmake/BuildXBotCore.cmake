a2c4af1ba69c25d41c9db42b3e7480586ecb0cd5
Luca Muratore <luca.muratore@iit.it>
2017-03-27 10:58:13 +0200

    Added XCM dependency on XBotCore.

--- cmake/BuildXBotCore.cmake
+++ cmake/BuildXBotCore.cmake
@@ -2,11 +2,11 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-find_or_build_package(XBotCoreModel QUIET)
+find_or_build_package(XCM QUIET)
+
 find_or_build_package(soem_advr QUIET)
 find_or_build_package(walkman-master-ecat QUIET)
 find_or_build_package(ec_master_test QUIET)
-find_or_build_package(sharedlibpp QUIET)
 
 ycm_ep_helper(  XBotCore
                 TYPE GIT
@@ -14,11 +14,10 @@ ycm_ep_helper(  XBotCore
                 REPOSITORY luca.muratore/xbotcore.git
                 TAG centauro_bonn_review_2016
                 COMPONENT external
-                DEPENDS XBotCoreModel
+		DEPENDS XCM
 			soem_advr
 			walkman-master-ecat
 			ec_master_test
-			sharedlibpp	
 		CMAKE_CACHE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON
                                  -DENABLE_XENO:BOOL=ON
 
